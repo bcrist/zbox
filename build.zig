21c409b46ada7070647e8a563d417e12de668d93
Ben Crist <github@magicmoremagic.com>
2024-01-12 21:26:15 -0600

    update for zig build system changes

--- build.zig
+++ build.zig
@@ -3,19 +3,17 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const deep_hash_map = b.dependency("Zig-DeepHashMap", .{}).module("deep_hash_map");
 
-    _ = b.addModule("zbox", .{
-        .source_file = .{ .path = "zbox.zig" },
-        .dependencies = &.{
-            .{ .name = "deep_hash_map", .module = deep_hash_map },
-        },
+    const zbox = b.addModule("zbox", .{
+        .root_source_file = .{ .path = "zbox.zig" },
     });
+    zbox.addImport("deep_hash_map", deep_hash_map);
 
     const tests = b.addTest(.{
         .root_source_file = .{ .path = "zbox.zig"},
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
     });
-    tests.addModule("deep_hash_map", deep_hash_map);
+    tests.root_module.addImport("deep_hash_map", deep_hash_map);
     const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run all tests");
     test_step.dependOn(&run_tests.step);
